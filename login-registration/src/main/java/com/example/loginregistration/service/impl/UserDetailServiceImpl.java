1735a7d4da02bdebbc79598af59cbd00ebddccf4
ugur ciftci <ugurprog@gmail.com>
2020-08-09 18:40:16 +0300

    Update UserDetailServiceImpl.java

--- login-registration/src/main/java/com/example/loginregistration/service/impl/UserDetailServiceImpl.java
+++ login-registration/src/main/java/com/example/loginregistration/service/impl/UserDetailServiceImpl.java
@@ -21,7 +21,6 @@ public class UserDetailServiceImpl implements UserDetailService {
 
     @Autowired
     private UserRepository userRepository;
-
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(s);

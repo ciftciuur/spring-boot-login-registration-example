eec4e26d0a1a6422a304bef6079f06e6aa0edcd5
ugur ciftci <ugurprog@gmail.com>
2020-08-10 21:25:12 +0300

    completed #6

--- login-registration/src/main/java/com/example/loginregistration/LoginRegistrationApplication.java
+++ login-registration/src/main/java/com/example/loginregistration/LoginRegistrationApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.example.loginregistration.repository")
 public class LoginRegistrationApplication {
 
     public static void main(String[] args) {
